7ab378e3fb623fe60158f9b1c5bfa65682e386a3
Rifas Mohamed <123630698+Rfs12@users.noreply.github.com>
2024-12-20 21:14:56 +0530

    Update skills.js

--- my-portfolio/src/components/Skills/skills.js
+++ my-portfolio/src/components/Skills/skills.js
@@ -5,27 +5,24 @@ const Skills = () => {
   return (
     <section id='skills'>
     <span className="skilltitle">What I do</span><br/>
-    <span className="skilldesc">I am a skilled and passionate web designer with experience</span>
+    <span className="skilldesc">I am a Skilled and Passionate Softwere Developer</span>
     <div className="skillbars">
         <div className='skillbar'>
             <img src={webimg} alt="Web design" className="skillbarimg"/>
             <div className="skillbartext">
                 <h2>Frontend Design</h2>
-                <p>This is a website for frontend design</p>
             </div>
         </div>
         <div className='skillbar'>
             <img src={webimg} alt="Fullstack" className="skillbarimg"/>
             <div className="skillbartext">
-                <h2>Fullstack Design</h2>
-                <p>This is a website with frontend and backend.</p>
+                <h2>Full Stack Design</h2>
             </div>
         </div>
         <div className='skillbar'>
             <img src={webimg} alt="App design" className="skillbarimg"/>
             <div className="skillbartext">
                 <h2>Mobile App Design</h2>
-                <p>Mobile App design this is a website</p>
             </div>
         </div>
     </div>
